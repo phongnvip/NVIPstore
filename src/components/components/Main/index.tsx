4686001f6774934624c50ef290934a04a56508b6
Nguyễn Văn Phong <thieugialee01@gmail.com>
2023-02-07 12:22:49 +0700

    responsive

--- src/components/components/Main/index.tsx
+++ src/components/components/Main/index.tsx
@@ -2,7 +2,7 @@ import Content from "./components/content";
 
 const Main = () => {
   return (
-    <div className="w-3/5 bg-transparent flex-col justify-center">
+    <div className="md:w-11/12 max-sm:w-11/12 w-3/5 bg-transparent flex-col justify-center">
       <Content />
       <Content />
       <Content />
--- src/components/components/index.tsx
+++ src/components/components/index.tsx
@@ -2,7 +2,7 @@ import Main from "./Main";
 
 const Home = () => {
   return (
-    <div className="h-auto w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
+    <div className="2xl:w-full xl:w-full lg:w-full md:w-full sm:w-full h-auto w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
       <div className="flex justify-center">
         <Main />
       </div>
